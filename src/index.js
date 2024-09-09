bb50871cd16a510a8ad3d2ce6043cae07d788723
stanley2058 <stanley2058@yahoo.com.tw>
2024-09-09 12:09:16 +0800

    feat: udpate tsconfig & exports

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 export * from './ws.js'
+export * from './socketio.js'
 export * from './server.js'
+export * from './y-socket-io/index.js'
---
+++ src/y-socket-io/index.js
@@ -0,0 +1,3 @@
+export * from './client.js'
+export * from './y-socket-io.js'
+export * from './user.js'
