dd93f3315c2bdcec8dac6d4e0b19496fc87f81c3
stanley2058 <stanley2058@yahoo.com.tw>
2025-01-20 11:32:18 +0800

    fix: check if stream is orphan on connection

--- src/y-socket-io/y-socket-io.js
+++ src/y-socket-io/y-socket-io.js
@@ -157,6 +157,10 @@ export class YSocketIO {
       this.initAwarenessListeners(socket)
       this.initSocketListeners(socket)
 
+      await this.client.checkAndRecoveryWorkerStream(
+        this.getNamespaceString(socket.nsp),
+        'index',
+      )
       const doc = await this.client.getDoc(namespace, 'index')
 
       if (
